6164c45038dc2f80d6fc6a3a965d41a8f4c78f37
shineunseo <shb5992@cu.ac.kr>
2023-12-01 01:24:34 +0000

    12.01

--- chap10/prob3/mem.c
+++ chap10/prob3/mem.c
@@ -1,37 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/shm.h>
-#include <sys/ipc.h>
 
 int main() {
-    key_t key = ftok("mem.c", 65);  
-    int shmid;
-    char *ptr;
-    char str1[] = "Do you like Linux?";
-    char str2[] = "like linux?";
+	char str[32] = "Do you like Linux?";
+	char *ptr, *p;
 
-    shmid = shmget(key, strlen(str1) + strlen(str2) + 10, IPC_CREAT | 0666);
-    if (shmid == -1) {
-        perror("shmget");
-        exit(1);
-    }
+	ptr = (char *) malloc(32);
+	memcpy(ptr, str, strlen(str));
+	puts(ptr);
+	memset(ptr + 12, 'l', 1);
+	puts(ptr);
 
-    ptr = (char *)shmat(shmid, NULL, 0);
-    if (ptr == (char *)(-1)) {
-        perror("shmat");
-        exit(1);
-    }
-
-    printf("%s\n", str1);
-    printf("%s\n", str1);
-    printf("%s\n", str2);
-    printf("%s\n", str1);
-
-    shmdt(ptr);
-
-    shmctl(shmid, IPC_RMID, NULL);
-
-    return 0;
+	p = (char *)memchr(ptr, 'l', 18);
+	puts(p);
+	memmove(str + 12, str + 7, 10);
+	puts(str);
 }
-
